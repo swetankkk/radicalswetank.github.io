a3c9060e4fd379c1381c73092e8ecd928e2a9fd5
Swetank <swetank199@gmail.com>
2020-10-05 02:11:39 +0530

    Updates

---
+++ precache-manifest.029d21460b2b88b9c85c87bbd778fc71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73a0dc1d87babf0f91fac0fcf04256ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2ef38be7d243625cfa9",
+    "url": "/static/css/main.68781335.chunk.css"
+  },
+  {
+    "revision": "4382c1838624f7b2eabe",
+    "url": "/static/js/2.a2bff88d.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/static/js/2.a2bff88d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2ef38be7d243625cfa9",
+    "url": "/static/js/main.48ee4976.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
