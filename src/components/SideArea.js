f71c21dd9638434c3011030336df5b58cceddd94
Swetank <swetank199@gmail.com>
2020-10-20 09:57:52 +0530

    Added intial Base for Left Bar

--- src/components/SideArea.js
+++ src/components/SideArea.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import style from './SideArea.style.js';
 const Area = () => {
-    return <div style={style.sidebar}>SideArea</div>;
+    return (
+      <div style={style.sidebar}>
+        <h1 style={style.head}>SWETANK</h1>
+        <div style={style.subhead}>Web Developer and Fintech Enthusiast</div>
+        <div>
+          <div style={style.hoverButtoms}>
+            About
+            <br />
+          </div>
+          <div style={style.hoverButtons}>
+            Skills
+            <br />
+          </div>
+          <div style={style.hoverButtons}>
+            Projects Talks
+            <br />
+          </div>
+          <div style={style.hoverButtons}>Certifications</div>
+        </div>
+      </div>
+    );
 };
 export default Area;
--- src/components/SideArea.style.js
+++ src/components/SideArea.style.js
@@ -1,9 +1,30 @@
 export default {
   sidebar: {
     backgroundColor: '#202020',
-        minWidth: '30vw',
-        minHeight: '85.1vh',
-        color:'white',
-//        color: 'rgba(255,255,255,.5)',
+    minWidth: '25vw',
+    minHeight: '73.1vh',
+    color: 'white',
+    fontSize: '20px',
+    paddingTop: '5rem',
+    paddingLeft: '3rem',
+    //        color: 'rgba(255,255,255,.5)',
   },
+  head: {
+    fontFamily: 'Montserrat, sans-serif',
+    fontSize: '55px',
+    fontWeight: 'bold',
+  },
+  subhead: {
+    fontFamily: 'Arial',
+    fontSize: '25px',
+    fontWeight: 'lighter',
+    //color: '#515151',
+    opacity: '0.7',
+    marginBottom: '1rem',
+  },
+  hoverButtons: {
+    lineHeight: '1.75rem',
+    
+  },
+  "hoverButtons:hover": { opacity: '1',  color: 'red' }
 };
